45e817e4d5bed3a5267e7d94c7604842e29c1b6a
Amaru <amaruvc@gmail.com>
2022-01-08 11:00:14 -0300

    correcciones de estilo (ina card por row)

--- public/desafio-instafake/assets/scripts.js
+++ public/desafio-instafake/assets/scripts.js
@@ -1,19 +1,35 @@
-let token;
+init();
+async function init() {
+  const token = localStorage.getItem("token");
+  if (token) {
+    getPosts(token);
+  }
+}
 $("#form-login").on("submit", async function (ev) {
   ev.preventDefault();
 
   const email = $("#email").val();
   const password = $("#password").val();
 
-  //ir a buscar el token
+  const token = await getToken(email, password);
+  const posts = await getPosts(token);
+  llenarCard(posts);
+});
+
+//ir a buscar el token
+async function getToken(email, password) {
   let jwt = await fetch("http://localhost:3100/api/login", {
     method: "POST",
     body: JSON.stringify({ email, password }),
   });
   jwt = await jwt.json();
   token = jwt.token;
+  localStorage.setItem("token", token);
+  return token;
+}
 
-  //buscar posteos
+//buscar posteos
+async function getPosts(token) {
   let data = await fetch("http://localhost:3100/api/photos", {
     method: "GET",
     headers: {
@@ -22,17 +38,15 @@ $("#form-login").on("submit", async function (ev) {
   });
   data = await data.json();
   const posts = data.data;
+  return posts;
+}
 
-  console.log(posts);
-  llenarLista(posts);
-});
-
-function llenarLista(posts) {
+function llenarCard(posts) {
   for (let post of posts) {
     $("#lista-post").append(`
-    <div class="col-3">
+    <div class="col-6 offset-3 pb-3">
         <div class="card">
-            <img src="${post.download_url}" class="card-img-top" style="height: 230px; object-fit: cover;" />
+            <img src="${post.download_url}" class="card-img-top" style="height: 400px; object-fit: cover;" />
             <div class="card-body">
             <p class="card-title">Autor: ${post.author}</p>
             </div>
