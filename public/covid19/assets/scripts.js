5ffe1ca5c6bce899814789636b916d0e4cbc0501
Amaru <amaruvc@gmail.com>
2022-01-13 17:58:47 -0300

    Se muestra grafico en el modal

--- public/covid19/assets/scripts.js
+++ public/covid19/assets/scripts.js
@@ -1,14 +1,22 @@
+//LLamamos a la función init a penas carga el archivo
 init();
+
+//Función init contempla que si un usuario que tiene token vigente todavía,
+//no le pida usuario y contraseña
+//Esto porque si tienen token vigente, significa que ya tienen su sesión iniciada
 async function init() {
   const token = localStorage.getItem("token");
   if (token) {
     ocultarLogin();
     let { masde10mil, paises } = await traerDatos(token);
-    chart(masde10mil);
+    chart("chartContainer", masde10mil);
     crearTabla(paises);
   }
 }
 
+//La Función getToken sirve para llamar a la API con un fetch (más usuario y contraseña)
+//y que nos devuelvuelva el token.
+//También hace que al llamar a API nos indique si hay error con el res.status
 async function getToken(email, password) {
   const res = await fetch("/api/login", {
     method: "POST",
@@ -24,20 +32,22 @@ async function getToken(email, password) {
   }
 }
 
-//traer datos
+//Función traer datos nos devuelve toda la información covid mundial en la variable paises
+//Y los países con más de 100000 muertes en la variable masde10mil
 async function traerDatos(token) {
   const data = await fetch("/api/total");
   const totalMundial = await data.json();
   const paises = totalMundial.data;
 
-  console.log(paises);
+  // console.log(paises);
   let masde10mil = paises.filter((f) => {
     return f.deaths >= 100000;
   });
   return { masde10mil, paises };
 }
 
-//crea la tabla
+//Esta función sirve para dibujar en la tabla cuando le paso un array
+//(en nuestro caso se llama paises el array)
 function crearTabla(array) {
   for (x of array) {
     $("#cuerpo-tabla").append(`
@@ -53,8 +63,11 @@ function crearTabla(array) {
   }
 }
 
-function chart(array) {
-  var chart = new CanvasJS.Chart("chartContainer", {
+//La creación del gráfico se pasó a esta función "Chart" que lo dibuja
+//Recibe por parámetro un array, que en este caso son los de la variable masde10mil
+function chart(selector, array) {
+  console.log(array);
+  var chart = new CanvasJS.Chart(selector, {
     animationEnabled: true,
     title: {
       text: "Países con Covid19",
@@ -147,6 +160,11 @@ function chart(array) {
   }
 }
 
+// Este es el Jquey del formulario de login.
+// En caso de que el usuario no tenga token, necesita identificarse con un usuario y contraseña válido
+// Al hacer click con datos correctos:
+// Este submit, crea un token a través de la función getToken y trae los datos con la función "traerDatos"
+// Además, llama a la función ocultar login, dibujar gráfico y creat tabla
 $("#form-login").on("submit", async (ev) => {
   ev.preventDefault();
   $("#cuerpo-tabla").empty();
@@ -163,45 +181,55 @@ $("#form-login").on("submit", async (ev) => {
     $("#email").val("");
     $("#password").val("");
     ocultarLogin();
-    chart(masde10mil);
+    chart("chartContainer", masde10mil);
     crearTabla(paises);
   } catch (err) {
     $("#login-error").removeClass("d-none").html(err.message);
   }
 });
 
-$(document).on("click", ".mostrarPais", (ev) => {
+async function traerDetallePais(pais) {
+  const data = await fetch(`/api/countries/${pais}`);
+  let paisData = await data.json();
+  paisData = paisData.data;
+
+  return paisData;
+}
+
+$(document).on("click", ".mostrarPais", async (ev) => {
   ev.preventDefault();
   $("#tituloModal").empty();
-  $("#modalBody").empty();
+  $("#chartPais").empty();
   let tr = $(ev.target).parent().parent();
   let tds = $(tr).find("td");
   const pais = tds[0].textContent;
+
+  let paisData = await traerDetallePais(pais);
+  paisData = [paisData];
+
   $("#tituloModal").prepend(`<h5 class="modal-title">${pais}</h5>`);
-  $("#modalBody").append(`<div>
-    <p class="pb-2"><strong>Datos Covid19:</strong></p>
-    <p>Casos confirmados: ${tds[1].textContent}</p>
-    <p>Casos fallecidos: ${tds[2].textContent}</p>
-    <p>Casos recuperados: ${tds[3].textContent}</p>
-    <p>Casos activos: ${tds[4].textContent}</p>
-  </div>`);
+  chart("chartPais", paisData);
 });
 
+//Esta función oculta el formulario de login y permite ser visibles al gráfico y tabla
 function ocultarLogin() {
   $("#div-form").removeClass("d-block").addClass("d-none");
   $(".datos").removeClass("d-none").addClass("d-block");
 }
 
+//Esta función oculta el gráfico y tabla y muestra el login (Es para cuando el usuario cierra sesión)
 function mostrarLogin() {
   $("#div-form").removeClass("d-none").addClass("d-block");
   $(".datos").removeClass("d-block").addClass("d-none");
 }
 
+//Función que elimina el token
 function logout() {
   localStorage.removeItem("token");
   mostrarLogin();
 }
 
+//Enlace del menú para cerrar sesión, llama a la función logout que se encarga de eliminar el token
 $("#logout").on("click", function (ev) {
   logout();
 });
