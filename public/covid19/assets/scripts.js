4a172ffbefd8d647db35950ca25f18e313ed40c6
Amaru <amaruvc@gmail.com>
2022-01-12 21:39:44 -0300

    tabla y login con token

--- public/covid19/assets/scripts.js
+++ public/covid19/assets/scripts.js
@@ -1,3 +1,54 @@
+init();
+async function init() {
+  const token = localStorage.getItem("token");
+  if (token) {
+    ocultarLogin();
+    let { masde10mil, paises } = await traerDatos(token);
+    chart(masde10mil);
+    crearTabla(paises);
+  }
+}
+
+async function getToken(email, password) {
+  let jwt = await fetch("/api/login", {
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+  });
+  jwt = await jwt.json();
+  token = jwt.token;
+  localStorage.setItem("token", token);
+  return token;
+}
+
+//traer datos
+async function traerDatos(token) {
+  const data = await fetch("/api/total");
+  const totalMundial = await data.json();
+  const paises = totalMundial.data;
+
+  console.log(paises);
+  let masde10mil = paises.filter((f) => {
+    return f.deaths >= 100000;
+  });
+  return { masde10mil, paises };
+}
+
+//crea la tabla
+function crearTabla(array) {
+  for (x of array) {
+    $("#cuerpo-tabla").append(`
+            <tr>
+                <td>${x.location}</td>
+                <td>${x.confirmed}</td>
+                <td>${x.deaths}</td>
+                <td>${x.recovered}</td>
+                <td>${x.active}</td>
+                <td><button type="button" class="btn btn-link mostrarPais" data-bs-toggle="modal" data-bs-target="#modalPais">Ver Más</button></td>
+            </tr>
+            `);
+  }
+}
+
 function chart(array) {
   var chart = new CanvasJS.Chart("chartContainer", {
     animationEnabled: true,
@@ -94,19 +145,39 @@ function chart(array) {
 
 $("#form-login").on("submit", async (ev) => {
   ev.preventDefault();
-  let { masde10mil } = await traerDatos();
+  const email = $("#email").val();
+  const password = $("#password").val();
+
+  const token = await getToken(email, password);
+  let { masde10mil, paises } = await traerDatos();
+
   chart(masde10mil);
+  crearTabla(paises);
+  ocultarLogin();
 });
 
-//traer datos
-async function traerDatos() {
-  const data = await fetch("/api/total");
-  const totalMundial = await data.json();
-  const paises = totalMundial.data;
+$(document).on("click", ".mostrarPais", (ev) => {
+  ev.preventDefault();
 
-  console.log(paises);
-  let masde10mil = paises.filter((f) => {
-    return f.deaths >= 100000;
-  });
-  return { masde10mil, paises };
+  $("#tituloModal").append(`<h5 class="modal-title">Argentina</h5>`);
+  $("#modalBody").append(`<p>Casos covid</p>`);
+});
+
+function ocultarLogin() {
+  $("#div-form").removeClass("d-block").addClass("d-none");
+  $(".container").removeClass("d-none").addClass("d-block");
 }
+
+function mostrarLogin() {
+  $("#div-form").removeClass("d-none").addClass("d-block");
+  $(".container").removeClass("d-block").addClass("d-none");
+}
+
+function logout() {
+  localStorage.removeItem("token");
+  mostrarLogin();
+}
+
+$("#logout").on("click", function (ev) {
+  logout();
+});
